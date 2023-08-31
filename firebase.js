846bc13d015b44bdfe654a42b9f5bf238ed1683c
pateljenil139 <94772560+pateljenil139@users.noreply.github.com>
2023-08-31 15:45:45 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,13 +4,14 @@ import { getAuth } from 'firebase/auth';
 
 // Your Firebase configuration object
 const firebaseConfig = {
-  apiKey: "AIzaSyAxCrIk45ix4byBuhcvQntuBp__rTdnTf8",
-  authDomain: "app-f222a.firebaseapp.com",
-  projectId: "app-f222a",
-  storageBucket: "app-f222a.appspot.com",
-  messagingSenderId: "617015014258",
-  appId: "1:617015014258:web:41d37c52be93a52cdb63eb",
-  measurementId: "G-QRQL54EK2M"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+  //add your firebase data 
 };
 const firebaseApp = initializeApp(firebaseConfig);
 
